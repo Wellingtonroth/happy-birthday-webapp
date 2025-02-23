70cc2a606cc9f3b10fb4ce8f492f3467ba1e0aee
Wellington Roth <wellingtonrothh@gmail.com>
2025-02-23 11:40:27 -0300

    baseurl example

---
+++ src/services/baseUrl.js
@@ -0,0 +1,8 @@
+const serverUrl = 'http://localhost:5000'; // This should match your backend server's URL
+// example const server = 'https://myapp.herokuapp.com';
+
+const base = {
+  example: `${serverUrl}/api/example`,
+};
+
+export default base;
