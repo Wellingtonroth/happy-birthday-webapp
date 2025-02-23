da51d5af3e8f9a40b1b3070e07dea8fbdb84110e
Wellington Roth <wellingtonrothh@gmail.com>
2025-02-23 11:39:49 -0300

    birthday module

---
+++ src/components/modules/birthday/ModuleBirthday.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="text-center p-10">
+    <h1 class="text-4xl font-bold text-green-500">🎂 Happy Birthday, [Name]! 🎉</h1>
+    <p>Welcome to your personalized birthday page!</p>
+  </div>
+</template>
+
+
+<script setup>
+import { ref } from 'vue'
+</script>
