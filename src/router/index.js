552ba2b96a867b3b8ab125d815e0eda899111d76
Wellington Roth <wellingtonrothh@gmail.com>
2025-03-03 10:22:10 -0300

    router beforeEnter to handle if order is pending

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LandingPage from "../views/LandingPageView.vue";
 import Checkout from "../views/CheckoutView.vue";
+import CheckoutPending from "../views/CheckoutPendingView.vue";
 import Birthday from "../views/BirthdayView.vue";
+import useBirthday from "../composables/useBirthday";
 
 const routes = [
-  { 
+  {
     path: "/", 
     component: LandingPage
   },
@@ -13,9 +15,28 @@ const routes = [
     component: Checkout
   },
   { 
-    path: "/birthday/:id", 
-    component: Birthday
+    path: "/checkout/pending",
+    component: CheckoutPending
   },
+  { 
+    path: "/birthday/:id", 
+    component: Birthday,
+    beforeEnter: async (to, from, next) => {
+      const { getOrderById } = useBirthday();
+      
+      const orderId = to.params.id; 
+      if (!orderId) {
+        console.error("Order ID is missing in route parameters.");
+        return next("/checkout/pending");
+      }
+
+      const order = await getOrderById(orderId);
+      if (order && order?.data?.is_paid) {
+        return next();
+      }
+      return next("/checkout/pending");
+    },
+  }
 ];
 
 const router = createRouter({
