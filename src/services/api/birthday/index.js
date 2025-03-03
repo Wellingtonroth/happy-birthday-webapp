dfa5df6ad505e730e7009093735ebbb2faf8705a
Wellington Roth <wellingtonrothh@gmail.com>
2025-03-03 16:36:25 -0300

    remove console

--- src/services/api/birthday/index.js
+++ src/services/api/birthday/index.js
@@ -3,7 +3,6 @@ import api from '../../axios';
 
 export default {
   getOrderById(payload) {
-    console.log('payload', payload);
     const uriGet = baseUrl.getOrderById
       .replace(':id', payload);
 
