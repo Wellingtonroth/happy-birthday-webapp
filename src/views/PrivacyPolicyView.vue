36cfbb2c417fa54bc4af4ed690bd1a8405a44ae4
Wellington Roth <wellingtonrothh@gmail.com>
2025-03-04 11:05:13 -0300

    nnew view, terms of use and privacy  policy

---
+++ src/views/PrivacyPolicyView.vue
@@ -0,0 +1,9 @@
+<template>
+  privacy policy
+  
+  <Footer />
+</template>
+
+<script setup>
+import Footer from '../components/shared/Footer.vue';
+</script>
---
+++ src/views/TermsOfUseView.vue
@@ -0,0 +1,9 @@
+<template>
+  terms of use
+  
+  <Footer />
+</template>
+
+<script setup>
+import Footer from '../components/shared/Footer.vue';
+</script>
