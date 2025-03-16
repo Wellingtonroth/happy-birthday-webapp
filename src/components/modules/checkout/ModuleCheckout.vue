8fdd937451879a51eb9fa6abdf13a66668a90627
Wellington Roth <wellingtonrothh@gmail.com>
2025-03-16 15:33:56 -0300

    add navigation in tha checkout page

--- src/components/modules/checkout/ModuleCheckout.vue
+++ src/components/modules/checkout/ModuleCheckout.vue
@@ -1,9 +1,15 @@
 <template>
-  <CheckoutForm />
+  <div>
+    <Navigation />
+  </div>
+  <CheckoutForm class="mt-30 mb-20"/>
+  <Footer />
 </template>
 
 <script setup>
 import CheckoutForm from "./CheckoutForm.vue";
+import Navigation from '../../shared/Navigation.vue';
+import Footer from '../../shared/Footer.vue';
 </script>
 
 
