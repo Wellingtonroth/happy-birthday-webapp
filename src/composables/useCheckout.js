607e352f562485ce553cc91a66b3395a4cf0b4ca
Wellington Roth <wellingtonrothh@gmail.com>
2025-03-25 18:45:34 -0300

    setOrder and GetTemplates

--- src/composables/useCheckout.js
+++ src/composables/useCheckout.js
@@ -3,6 +3,13 @@ import { useCheckoutStore } from "../store/checkout";
 
 export default function useCheckout(checkoutStore = useCheckoutStore()) {
   const isLoading = computed(() => checkoutStore.isLoading);
+  const templates = computed(() => checkoutStore.templates);
+
+  const selectedTemplate = computed(() => checkoutStore.selectedTemplate);
+  const setSelectedTemplate = (template) => checkoutStore.setSelectedTemplate(template);
+
+  const order = computed(() => checkoutStore.order);
+  const setOrder = (payload) => checkoutStore.setOrder(payload);
 
   const createOrder = async (payload) => {
     return await checkoutStore.createOrder(payload);
@@ -12,9 +19,20 @@ export default function useCheckout(checkoutStore = useCheckoutStore()) {
     return await checkoutStore.uploadImages(orderId, files);
   };
 
+  const getTemplateImages = async () => {
+    return await checkoutStore.getTemplateImages();
+  };
+
   return {
     isLoading,
+    templates,
+    selectedTemplate,
+    order,
+
     createOrder,
     uploadImages,
+    getTemplateImages,
+    setSelectedTemplate,
+    setOrder,
   };
 }
