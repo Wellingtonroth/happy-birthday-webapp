8ff5fa760d754239bba8e0ea6245c10a032eb1c6
Wellington Roth <wellingtonrothh@gmail.com>
2025-04-03 16:37:11 -0300

    add i18n to navigation

--- src/components/shared/Navigation.vue
+++ src/components/shared/Navigation.vue
@@ -1,9 +1,13 @@
 <template>
   <nav class="fixed top-0 left-0 w-full flex justify-between items-center py-3 px-4 lg:px-10 bg-white shadow-md z-50">
     <router-link to="/" class="text-lg md:text-2xl font-semibold text-[#2D3436]">
-      🎉 HappyBirthday
+      {{ $t('shared.component.navigation.home') }}
     </router-link>
-    <CTAButton label="Create My Site" variant="secondary" class="text-sm px-4 py-2 md:px-6 md:py-3 md:text-lg" />
+    <CTAButton 
+      :label="$t('shared.component.button.create')" 
+      variant="secondary" 
+      class="text-sm px-4 py-2 md:px-6 md:py-3 md:text-lg"
+    />
   </nav>
 </template>
 
