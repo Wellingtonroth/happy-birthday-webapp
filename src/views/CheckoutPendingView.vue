8dcf8123e3f9980b5c8c59d61eebf0a3c5d7860f
Wellington Roth <wellingtonrothh@gmail.com>
2025-04-04 14:46:53 -0300

    feat: :sparkles: peding page

--- src/views/CheckoutPendingView.vue
+++ src/views/CheckoutPendingView.vue
@@ -1,9 +1,19 @@
 <template>
-  <div>
-    finalize o pagamento para visualizar
+  <div class="flex flex-col items-center justify-center min-h-screen px-4 text-center">
+    <h1 class="text-3xl font-bold mb-4">
+      {{ t('module.checkout.pending.title') }}
+    </h1>
+    <p class="text-lg mb-6">
+      {{ t('module.checkout.pending.description') }}
+    </p>
+    <p class="text-sm text-gray-500 mb-8">
+      {{ t('module.checkout.pending.information') }}
+    </p>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
 </script>
